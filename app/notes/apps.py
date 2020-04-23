a390478e1b551ad4fc7671a4650675fab18b911e
Vovchenko Nikita <vovchenko.nikita.a@gmail.com>
2020-04-23 14:44:22 +0300

    notes,apps: update NoteApp to scrallable text inputs.

--- app/notes/apps.py
+++ app/notes/apps.py
@@ -2,7 +2,8 @@ import requests
 from kivy.app import App
 from kivy.uix.carousel import Carousel
 from kivy.uix.gridlayout import GridLayout
-from kivy.uix.label import Label
+from kivy.uix.scrollview import ScrollView
+from kivy.uix.textinput import TextInput
 
 import app.commons
 import app.settings
@@ -10,24 +11,25 @@ import app.settings
 
 class NoteApp(App):  # type: ignore
     def build(self) -> Carousel:
-        carousel = Carousel(direction="top")
+        scroll_view = ScrollView(do_scroll_y=True)
+        grid_layout = GridLayout(cols=1, size_hint_y=10)
+        grid_layout.bind(minimum_height=grid_layout.setter('height'))
 
         note_bytes = requests.get(app.settings.DATA_URL)
         note_list = app.commons.msgpack.loads(note_bytes.content)
 
         for note in note_list:
-
             tags_grid = GridLayout(rows=len(note["tags"]))
             for tag in note["tags"]:
-                label = Label(text=tag["name"], markup=True)
+                label = TextInput(text=tag["name"])
                 tags_grid.add_widget(label)
 
-            label = Label(text=note["text"], markup=True)
+            label = TextInput(text=note["text"])
 
             grid = GridLayout(cols=2)
             grid.add_widget(label)
             grid.add_widget(tags_grid)
+            grid_layout.add_widget(grid)
 
-            carousel.add_widget(grid)
-
-        return carousel
+        scroll_view.add_widget(grid_layout)
+        return scroll_view
--- app/settings.py
+++ app/settings.py
@@ -1,1 +1,1 @@
-DATA_URL = "http://192.168.110.88:8080/api/v1/note/read/"
+DATA_URL = "http://192.168.1.95:8080/api/v1/note/read/"
